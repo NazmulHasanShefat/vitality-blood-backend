260999e8e1df5ccf8303ab0ff1b3f40729259e3a
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-06-22 18:18:48 +0600

    added update query

--- controllers/donation.js
+++ controllers/donation.js
@@ -1,18 +1,87 @@
-const { getCollection } = require("../db/dbConnection.js")
+const { ObjectId } = require("mongodb");
+const { getCollection } = require("../db/dbConnection.js");
 
-const createDonation = async (req, res)=>{
-    const collectionInfo = req.body;
-    try {
-        const newRequest = {
-            ...collectionInfo,
-            createdAt: new Date()
-        }
-        const requestCollection = await getCollection("request");
-        const result = await requestCollection.insertOne(newRequest);
-        res.json(result)
-    } catch (error) {
-        console.log(error)
+const createDonation = async (req, res) => {
+  const collectionInfo = req.body;
+  try {
+    const newRequest = {
+      ...collectionInfo,
+      createdAt: new Date(),
+    };
+    const requestCollection = await getCollection("request");
+    const result = await requestCollection.insertOne(newRequest);
+    return res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getDonorDonationRequests = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = { requesterId: id };
+
+    const requestCollection = await getCollection("request");
+    const result = await requestCollection.find(query).toArray();
+    return res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getRecentDonorDonationRequests = async (req, res) => {
+  const { id } = req.params;
+  const limit = req.query;
+  try {
+    const query = { requesterId: id };
+
+    const requestCollection = await getCollection("request");
+    const result = await requestCollection.find(query).sort({ _id: -1 }).limit(3).toArray();
+    return res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getDonationDetails = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const requestCollection = await getCollection("request");
+    const result = await requestCollection.findOne(query);
+    return res.json(result);
+  } catch (error) {
+   return console.log(error);
+  }
+};
+
+const updateDonationRequest = async(req, res)=>{
+  const {id} = req.params;
+  const updatedDocument = req.body;
+  const newDocument = {
+    ...updatedDocument,
+    updatedAt: new Date()
+  }
+
+  const newUpdatedDocument = {
+    $set:{
+      ...newDocument
     }
+  }
+  try {
+     const query = { _id: new ObjectId(id) };
+    const requestCollection = await getCollection("request");
+    const result = await requestCollection.updateOne(query, newUpdatedDocument);
+    return res.json(result);
+  } catch (error) {
+    return console.log(error)
+  }
 }
 
-module.exports = {createDonation}
+module.exports = { 
+    createDonation,
+    getDonorDonationRequests,
+    getDonationDetails,
+    updateDonationRequest,
+    getRecentDonorDonationRequests
+ };
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { createDonation } = require("../controllers/donation.js");
+const { createDonation, getDonorDonationRequests, getDonationDetails, updateDonationRequest, getRecentDonorDonationRequests } = require("../controllers/donation.js");
 const appRouter = express.Router();
 
-appRouter.post("/create-donatio-request", createDonation)
+appRouter.post("/create-donatio-request", createDonation);
+appRouter.get("/get-donor-donation-request/:id", getDonorDonationRequests);
+appRouter.get("/get-donation-detail/:id", getDonationDetails);
+appRouter.patch("/update-donation-request/:id", updateDonationRequest);
+appRouter.get("/get-recent-donor-request", getRecentDonorDonationRequests);
+
 
 module.exports = {appRouter}
