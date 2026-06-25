719f2b3b5889baeca2216ddf08b64b2be8f5551a
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-06-25 14:09:14 +0600

    added middle ware all routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,17 +5,17 @@ const { updateProfile } = require("../controllers/user.js");
 const { verifyUser, verifyAdmin } = require("../middleware/verifyUser.js");
 const appRouter = express.Router();
 
-appRouter.post("/create-donatio-request", createDonation);
-appRouter.get("/get-donor-donation-request/:id", getDonorDonationRequests);
-appRouter.get("/get-donation-detail/:id", getDonationDetails);
-appRouter.patch("/update-donation-request/:id", updateDonationRequest);
-appRouter.get("/get-recent-donor-request/:id", getRecentDonorDonationRequests);
-appRouter.patch("/update-donation-status/:id", updateDonationStatus);
-appRouter.delete("/delete-donation-request/:id", deleteDonationRequest);
-appRouter.get("/filter-donation-request/:id", filterDonationRequest);
+appRouter.post("/create-donatio-request", verifyUser, createDonation);
+appRouter.get("/get-donor-donation-request/:id", verifyUser, getDonorDonationRequests);
+appRouter.get("/get-donation-detail/:id", verifyUser, getDonationDetails);
+appRouter.patch("/update-donation-request/:id", verifyUser, updateDonationRequest);
+appRouter.get("/get-recent-donor-request/:id", verifyUser, getRecentDonorDonationRequests);
+appRouter.patch("/update-donation-status/:id", verifyUser, updateDonationStatus);
+appRouter.delete("/delete-donation-request/:id", verifyUser, deleteDonationRequest);
+appRouter.get("/filter-donation-request/:id", verifyUser, filterDonationRequest);
 appRouter.get("/all-donation-requests", verifyUser, verifyAdmin, getAllDonationRequest);
-appRouter.post("/createFundingHistory", createfundingHistory);
-appRouter.patch("/update-user/:id", updateProfile);
+appRouter.post("/createFundingHistory", verifyUser, createfundingHistory);
+appRouter.patch("/update-user/:id", verifyUser, updateProfile);
 appRouter.get("/get-pending-blood-donation-request", verifyUser, getPedingBloodDonationRequest)
 
 module.exports = {appRouter}
